80ba7158b79190ca673cd37688d11eb758c02f0b
Will <will@Will.(none)>
2012-12-12 12:32:57 +0100

    tighter packing, flipped map, 5.3K

--- map_v4_data.py
+++ map_v4_data.py
@@ -43,8 +43,8 @@ dup, code = chr(code), code+1
 data = ""
 prev = [[None]*width for _ in xrange(height)]
 for year in xrange(len(years)):
-    for x in xrange(width): # this leads to better RLE compression, doing column order
-        for y in xrange(height):
+    for x in xrange(width-1,-1,-1): # this leads to better RLE compression, doing column order, and flipped
+        for y in xrange(height-1,-1,-1):
             row = rows[1+(x*height)+y+1].split("\t")
             assert int(row[0]) == x+1, (x,y,row)
             assert int(row[1]) == y+1, (x,y,row)
@@ -56,13 +56,15 @@ for year in xrange(len(years)):
             data += religion
 assert len(data) == len(years)*height*width
 print "map",len(data),hu(len(data))
-print "checksum",sum(ord(ch) for ch in data)
+checksum = sum(ord(ch) for ch in data)
+print "checksum",checksum
+assert checksum == 55009555
 
 # now RLE it
 ofs, rle = 0, ""
-run_1, run_2, run_3, code = chr(code), chr(code+1), chr(code+2), code+3
-run_zero = None #UPPER-code # set to None to disable
-MIN, MAX = 2, RANGE**2 # fun fact: swapping out (code,code) for (code,rle-1) gives the next stage more to work on
+run_1, run_2, code = chr(code), chr(code+1), code+2
+MIN = 1 # fun fact: swapping out (code,code) for (code,rle-1) gives the next stage more to work on
+MAX = MIN+RANGE**2 
 while ofs < len(data):
     run = 0
     while ofs+run+1 < len(data) and run < MAX:
@@ -72,9 +74,8 @@ while ofs < len(data):
             break
     rle += data[ofs]
     if run >= MIN:
-        if run_zero and run < run_zero:
-            rle += chr(code+run)
-        elif run < RANGE:
+        run -= MIN+1
+        if run < RANGE:
             rle += run_1
             rle += chr(LOWER+run)
         elif run < RANGE*RANGE:
@@ -82,11 +83,8 @@ while ofs < len(data):
             rle += chr(LOWER+(run//RANGE))
             rle += chr(LOWER+(run%RANGE))
         else:
-            rle += run_3
-            rle += chr(LOWER+((run//(RANGE*RANGE))))
-            rle += chr(LOWER+((run//RANGE)%RANGE))
-            rle += chr(LOWER+(run%RANGE))
-        ofs += run+1
+            sys,exit("bad run! "+run);
+        ofs += run+1+MIN+1
     else:
         ofs += 1
 print "rle",len(rle),hu(len(rle))
@@ -175,35 +173,27 @@ else:
     print rle
     sys.exit("decompression failed!")
 unrle = ""
-ofs, prev = 0, None
+ofs, last = 0, None
 while ofs < len(uncompressed):
     ch = uncompressed[ofs]
     ofs += 1
-    run = 1
     if ch == run_1:
-        ch = prev
         run = ord(uncompressed[ofs])-LOWER
+        run += MIN+1
         ofs += 1
     elif ch == run_2:
-        ch = prev
         run = (ord(uncompressed[ofs])-LOWER)*RANGE
         run += ord(uncompressed[ofs+1])-LOWER
+        run += MIN+1
         ofs += 2
-    elif ch == run_3:
-        ch = prev
-        run = (ord(uncompressed[ofs])-LOWER)*RANGE*RANGE
-        run += (ord(uncompressed[ofs+1])-LOWER)*RANGE
-        run += ord(uncompressed[ofs+2])-LOWER
-        ofs += 3
-    elif ord(ch) > code:
-        assert run_zero
-        run = ord(ch)-code
-        ch = prev
-    for i in xrange(run):
+    else:
         unrle += ch
-    prev = ch
+        last = ch
+        continue
+    for i in xrange(run):
+        unrle += last
+    #print "###",ofs,prev,ch,ch==run_1,ch==run_2,ord(ch)>=code,run
     if not data.startswith(unrle):
-        print "###",ofs,prev,ch,ch==run_1,ch==run_2,ch==run_3,ord(ch)>code,run
         print unrle
         print data[:len(unrle)]
         sys.exit("un-rle failed!")
